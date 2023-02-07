e552a806fd19098c9d0e1a62375b2fa0ec3f51f4
sunjae <1019lsj@naver.com>
2023-02-07 18:15:49 +0900

    23.02.07

--- 3.object/8.2bind.js
+++ 3.object/8.2bind.js
@@ -1,1 +1,29 @@
-let user
+let user = {
+    userName: 'leo'
+}
+
+function greet() {
+    console.log(`I am ${this.userName}.`)
+}
+
+greet()
+
+let fn = greet.bind(user)
+fn() 
+
+setTimeout(fn, 100)
+
+user = {
+    userName:'john',
+    greet() {
+        console.log(`I am ${this.userName}.`)
+    }
+}
+
+// fn 에 user.greet 을 할당하라
+//       greet의 this 엔 user가 담겨 있다
+fn = user.greet.bind(user)
+setTimeout(fn, 200)
+
+console.log(fn == user.greet)
+
