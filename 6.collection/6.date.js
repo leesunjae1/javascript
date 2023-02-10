145d3aea06a91ca694a77d4e8238d3cc120c8b4e
sunjae <1019lsj@naver.com>
2023-02-10 11:29:27 +0900

    23.02.10

---
+++ 6.collection/6.date.js
@@ -0,0 +1,35 @@
+console.log(Date())
+console.log(typeof Date())
+
+let date = new Date()
+console.log(date)
+console.log(typeof date)
+console.log(date.toLocaleDateString())
+
+date = new Date('2023-02-10T01:37:30.999Z') //utc시각으로 한국이랑 9시간 차이난다
+console.log(date.toLocaleDateString())
+console.log(date.toLocaleTimeString())
+
+console.log(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    date.getUTCHours(),
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+    date.getMilliseconds(),
+    date.getDay()
+)
+//timestamp
+console.log(date.getTime())
+console.log(Date.now())
+
+date = new Date(0)
+console.log(date)
+
+date = new Date(1000 * 60 * 60 * 24)
+console.log(date)
+
+date = new Date(-1000 * 60 * 60 * 24)
+console.log(date)
---
+++ 7.module/2.default.mjs
@@ -0,0 +1,4 @@
+const divide = (a, b) => a / b
+const e = 1
+
+export default divide //1개만 export
